4f2165b954f068b05571fa096b54f9d7ea73a4c4
Fabrício <guilhermo.fabricio@gmail.com>
2020-03-27 16:25:46 -0300

    Add interface

---
+++ mapasiOS/mapasiOS/ViewController/MainViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MainViewController.swift
+//  mapasiOS
+//
+//  Created by Fabrício Guilhermo on 27/03/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    private lazy var destinationTextField: UITextField = {
+        let destinationTextField = UITextField(frame: .zero)
+        destinationTextField.translatesAutoresizingMaskIntoConstraints = false
+        destinationTextField.placeholder = "Local"
+        destinationTextField.textContentType = .telephoneNumber
+        destinationTextField.keyboardType = .alphabet
+        destinationTextField.clearButtonMode = .always
+        destinationTextField.delegate = self
+
+        return destinationTextField
+    }()
+
+    private lazy var searchAddressOnMapsButton: UIButton = {
+        let searchAddressOnMapsButton = UIButton(type: .roundedRect)
+        searchAddressOnMapsButton.translatesAutoresizingMaskIntoConstraints = false
+        searchAddressOnMapsButton.setTitle("Ver ponto no maps", for: .normal)
+        searchAddressOnMapsButton.layer.cornerRadius = 35
+        searchAddressOnMapsButton.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+        searchAddressOnMapsButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        searchAddressOnMapsButton.addTarget(self, action: #selector(searchAddressOnMapsButtonAction(_ :)), for: .touchUpInside)
+
+        return searchAddressOnMapsButton
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        view.addSubview(destinationTextField)
+        view.addSubview(searchAddressOnMapsButton)
+        autoLayout()
+
+    }
+
+    @objc private func searchAddressOnMapsButtonAction(_ sender: UIButton) {
+        let vc = MapViewController()
+        present(vc, animated: true, completion: nil)
+    }
+}
+
+extension MainViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
+
+extension MainViewController {
+    private func autoLayout() {
+        destinationTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        destinationTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        destinationTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        destinationTextField.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        searchAddressOnMapsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchAddressOnMapsButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        searchAddressOnMapsButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+        searchAddressOnMapsButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        searchAddressOnMapsButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -40).isActive = true
+    }
+}
+
---
+++ mapasiOS/mapasiOS/ViewController/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  mapasiOS
+//
+//  Created by Fabrício Guilhermo on 27/03/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    private lazy var closeButton: UIButton = {
+        let closeButton = UIButton(type: .roundedRect)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("x", for: .normal)
+        closeButton.layer.cornerRadius = 20
+        closeButton.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+        closeButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        closeButton.addTarget(self, action: #selector(closeButtonAction(_ :)), for: .touchUpInside)
+
+        return closeButton
+    }()
+
+    private lazy var map: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        map.layer.cornerRadius = 20
+
+        return map
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        view.addSubview(closeButton)
+        view.addSubview(map)
+
+        autoLayout()
+    }
+
+    @objc private func closeButtonAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension MapViewController {
+    private func autoLayout() {
+        closeButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        closeButton.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        closeButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        closeButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20).isActive = true
+
+        map.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        map.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        map.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        map.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+    }
+}
